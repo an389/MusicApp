9a01edda3507aaad104498880a7212ebba1d6d54
Mihai Andrei-Iulian <62914848+an389@users.noreply.github.com>
2023-01-10 10:14:08 +0200

    Play music from firebase

--- app/src/main/java/com/example/musicapp/Client/Adapter/MusicListAdapterUrl.java
+++ app/src/main/java/com/example/musicapp/Client/Adapter/MusicListAdapterUrl.java
@@ -3,28 +3,34 @@ package com.example.musicapp.Client.Adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.musicapp.Model.AudioModel;
+import com.example.musicapp.Model.GetSongs;
 import com.example.musicapp.Model.MyMediaPlayer;
 import com.example.musicapp.Client.MusicPlayerActivity;
 import com.example.musicapp.Model.Upload;
 import com.example.musicapp.R;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class MusicListAdapter extends RecyclerView.Adapter<MusicListAdapter.ViewHolder>{
+public class MusicListAdapterUrl extends RecyclerView.Adapter<MusicListAdapterUrl.ViewHolder>{
 
-    ArrayList<AudioModel> songsList;
+    ArrayList<GetSongs> songsList;
     Context context;
+    MediaPlayer mediaPlayer;
 
-    public MusicListAdapter(ArrayList<AudioModel> songsList, Context context) {
+    public MusicListAdapterUrl(ArrayList<GetSongs> songsList, Context context) {
         this.songsList = songsList;
         this.context = context;
     }
@@ -37,8 +43,8 @@ public class MusicListAdapter extends RecyclerView.Adapter<MusicListAdapter.View
 
     @Override
     public void onBindViewHolder( ViewHolder holder, int position) {
-        AudioModel songData = songsList.get(position);
-        holder.titleTextView.setText(songData.getTitle());
+        GetSongs songData = songsList.get(position);
+        holder.titleTextView.setText(songData.getSongTitle());
 
         if(MyMediaPlayer.currentIndex==position){
             holder.titleTextView.setTextColor(Color.parseColor("#FF0000"));
@@ -49,15 +55,29 @@ public class MusicListAdapter extends RecyclerView.Adapter<MusicListAdapter.View
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //navigate to another acitivty
+//                //navigate to another acitivty
+//
+//                MyMediaPlayer.getInstance().reset();
+//                MyMediaPlayer.currentIndex = 0;
+//                Intent intent = new Intent(context, MusicPlayerActivity.class);
+//                intent.putExtra("LIST",songsList);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                context.startActivity(intent);
 
-                MyMediaPlayer.getInstance().reset();
-                MyMediaPlayer.currentIndex = position;
-                Intent intent = new Intent(context, MusicPlayerActivity.class);
-                intent.putExtra("LIST",songsList);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(intent);
+                //play music
+                Toast.makeText(context, "Melodia" + songData.getSongLink(), Toast.LENGTH_SHORT).show();
 
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+                try {
+                    mediaPlayer.setDataSource(songData.getSongLink());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    Toast.makeText(context, "Playing audio", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(context, "Error occured = " + e, Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- app/src/main/java/com/example/musicapp/Client/ListWithSongActivity.java
+++ app/src/main/java/com/example/musicapp/Client/ListWithSongActivity.java
@@ -1,16 +1,90 @@
 package com.example.musicapp.Client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.musicapp.Client.Adapter.MusicListAdapter;
+import com.example.musicapp.Client.Adapter.MusicListAdapterUrl;
+import com.example.musicapp.Model.AudioModel;
+import com.example.musicapp.Model.GetSongs;
+import com.example.musicapp.Model.Upload;
 import com.example.musicapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListWithSongActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<GetSongs> mUpload;
+    DatabaseReference databaseReference;
+    ValueEventListener valueEventListener;
+    Boolean checkIn = false;
+    private int currentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_with_song);
+
+        mUpload = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycler_viewURL);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+     //   noMusicTextView = findViewById(R.id.no_songs_textURL);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("songs");
+        valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (!mUpload.isEmpty())
+//                    mUpload.clear();
+
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+
+                    GetSongs upload = dataSnapshot.getValue(GetSongs.class);
+                    mUpload.add(upload);
+//                    GetSongs getSongs = dss.getValue(GetSongs.class);
+//                    getSongs.setmKey(dss.getKey());
+//                    currentIndex = 0;
+//                    final String s = getIntent().getExtras().getString("songsCategory");
+//                    noMusicTextView.setText(getSongs.getSongsCategory());
+//
+//                    mUpload.add(getSongs);
+//                    if (s.equals(getSongs.getSongsCategory())){
+//                        mUpload.add(getSongs);
+//                        checkIn = true;
+//                        noMusicTextView.setText("e muzica");
+//                    }
+                }
+               // noMusicTextView.setText(mUpload.get(2).name.toString());
+                recyclerView.setAdapter(new MusicListAdapterUrl(mUpload,getApplicationContext()));
+//                if (mUpload.size() == 0){
+//                    noMusicTextView.setVisibility(View.VISIBLE);
+//                }else {
+//                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
